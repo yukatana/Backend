ba4482f03f2de325cb38420ed4e64c283cc8d6cf
yukatana <rommel.aranguren@gmail.com>
2022-08-01 19:35:50 -0300

    pug implementation

---
+++ template-engines/pug/server.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const PORT = 8080
+
+//Pug import:
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+//Container class import to be used by GET /products
+const Container = require("./utils/container")
+const file = "database.json"
+const container = new Container(file)
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+    console.log(`Express HTTP server running on port ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+    res.render("form.pug")
+})
+
+app.get("/products", async (req, res) => {
+    const data = await container.getAll()
+    res.render("grid.pug", {item: data, dataExists: data.length > 0 ? true : false})
+})
+
+app.post("/products", async (req, res) => {
+    await container.save({
+        name: req.body.name,
+        price: req.body.price,
+        thumbnail: req.body.thumbnail
+    })
+    res.redirect("/products")
+})
+
+server.on("error", error => console.log(`Server error: ${error}`))
