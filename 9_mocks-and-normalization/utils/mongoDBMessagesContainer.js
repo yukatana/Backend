43d70055e175d5d6388ebe53ba8ee425d2dd4c7d
yukatana <rommel.aranguren@gmail.com>
2022-09-14 11:50:09 -0300

    started normalizr testing

--- 9_mocks-and-normalization/utils/mongoDBMessagesContainer.js
+++ 9_mocks-and-normalization/utils/mongoDBMessagesContainer.js
@@ -18,27 +18,62 @@ module.exports = class mongoDBMessagesContainer {
 
     save = async (commentObject) => {
         try {
-            const author = new normalizr.schema.Entity('authors')
-            const message = new normalizr.schema.Entity('message', {}, {idAttribute: 'text'})
-            //commentSchema contains both of the above schemas
-            const comment = new normalizr.schema.Entity('comments', {
-                author: author,
-                message: message
+            const author = new normalizr.schema.Entity('author')
+            //comment schema contains both of the above schemas
+            const comments = new normalizr.schema.Entity('comment', {
+                author
             })
-            let data = await this.getAll()
-            data.comments = []
-            data.comments.push(commentObject)
-            data = {id: 'comments', ...data}
-            console.log('--------------------------------------------------')
+            // let data = await this.getAll()
+            // data.comments = []
+            // data.comments.push(commentObject)
+            // data = {id: 'comments', ...data}
+            let data = {
+                id: 'comments',
+                comments: [
+                    {
+                        author: {
+                            id: 'rommel.aranguren@gmail.com',
+                            name: '',
+                            last_name: '',
+                            age: '',
+                            alias: '',
+                            avatar: ''
+                        },
+                        message: { text: 'hello', dateString: '9/14/2022, 11:05:34 AM' }
+                    },
+                    {
+                        author: {
+                            id: 'rommel.aranguren@gmail.com',
+                            name: '',
+                            last_name: '',
+                            age: '',
+                            alias: '',
+                            avatar: ''
+                        },
+                        message: { text: 'hello', dateString: '9/14/2022, 11:05:34 AM' }
+                    },
+                    {
+                        author: {
+                            id: 'rommel.aranguren@gmail.com',
+                            name: '',
+                            last_name: '',
+                            age: '',
+                            alias: '',
+                            avatar: ''
+                        },
+                        message: { text: 'hello', dateString: '9/14/2022, 11:05:34 AM' }
+                    }
+                ]
+            }
             console.log(util.inspect(data, true, 5, true))
-            const normalizedData = normalizr.normalize(data, [comment])
+            console.log('--------------------------------------------------')
+            const normalizedData = normalizr.normalize(data, [comments])
             console.log('--------------------------------------------------')
             console.log(util.inspect(normalizedData, true, 5, true))
             // console.log('--------------------------------------------------')
             // const denormalizedData = normalizr.denormalize(normalizedData.result, commentSchema, normalizedData.entities)
             // console.log(util.inspect(denormalizedData, true, 5, true))
-
-            //return await new this.Schema(commentObject).save()
+            // return await new this.Schema(commentObject).save()
         } catch (err) {
             console.log(err)
         }
